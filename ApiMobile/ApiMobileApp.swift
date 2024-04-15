7a98cdac7caa0a0fbfa223e7a10310930d9d1bd3
Aiden <stichaiden2@gmail.com>
2024-04-15 08:37:31 -0500

    Initial Commit

---
+++ ApiMobile/ApiMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiMobileApp.swift
+//  ApiMobile
+//
+//  Created by STICH, AIDEN S. on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ApiMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
