87e4ed6863bba24a0ad00f2681951012d6f7ca46
Johnny <johsun240@gmail.com>
2024-04-15 08:59:13 -0500

    Add api

--- ApiMobile/ContentView.swift
+++ ApiMobile/ContentView.swift
@@ -17,7 +17,28 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+    
+    
+    func getApi(){
+         if let apiURL = URL(string:"https://api.github.com/search/users?q=greg"){
+             var request = URLRequest(url:apiURL)
+             request.httpMethod = "GET"
+             URLSession.shared.dataTask(with: request){
+                 data, response,error in
+                 if let apiData = data {
+                     if let usersFromAPI = try? JSONDecoder().decode(Result.self, from: apiData){
+                         data = usersFromAPI.items
+                         print(data)
+                     }
+                 }
+             }.resume()
+         }
+     }
+    
 }
+
+
 
 #Preview {
     ContentView()
